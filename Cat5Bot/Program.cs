37e85c6eb7f6db4c9d1287722ad339c9d80c2c21
Connor <61102529+Metater@users.noreply.github.com>
2021-10-28 08:38:16 -0400

    Add project

---
+++ Cat5Bot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+
+Console.WriteLine("Hello, World!");
+
+await MainAsync();
+
+static async Task MainAsync()
+{
+    string? token = Console.ReadLine();
+
+    var discord = new DiscordClient(new DiscordConfiguration()
+    {
+        Token = token,
+        TokenType = TokenType.Bot,
+        Intents = DiscordIntents.AllUnprivileged
+    });
+
+    discord.MessageCreated += async (s, e) =>
+    {
+        await e.Message.CreateReactionAsync(DiscordEmoji.FromName(discord, ":thumbup:"));
+    };
+
+    var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+    {
+        StringPrefixes = new[] { "!" }
+    });
+
+    //commands.RegisterCommands<Commands.GeneralModule>();
+
+    await discord.ConnectAsync();
+    await Task.Delay(-1);
+}

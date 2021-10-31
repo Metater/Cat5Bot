cc945b9030713811ab166104eed51942991d864e
Connor <61102529+Metater@users.noreply.github.com>
2021-10-30 22:59:21 -0400

    command modules

---
+++ Cat5Bot/Commands/PlanningModule.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using Cat5Bot.DB;
+
+namespace Cat5Bot.Commands;
+
+[Group("plan")]
+[Description("Used for planning attendable events.")]
+[Aliases("schedule", "create")]
+public class PlanningModule : BaseCommandModule
+{
+    [GroupCommand]
+    public async Task PlanGroupCommand(CommandContext ctx)
+    {
+
+    }
+}
